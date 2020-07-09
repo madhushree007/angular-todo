f2961022a95a279f0852f9c497049fd3e0d9d00d
Madhushree Gupta <madhushree25@gmail.com>
2020-07-09 14:02:08 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  name = 'Madhu';
+
+  constructor() {
+    this.changeName('John');
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
+
 }
